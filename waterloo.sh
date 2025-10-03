8a94f33e5d8941ecd54c08f1b51e4a3a28977546
Pratyush Balaji <59258572+PratyushBalaji@users.noreply.github.com>
2025-10-02 18:04:01 -0700

    waterloo.sh (As of 10-02-2025)

--- waterloo.sh
+++ waterloo.sh
@@ -10,6 +10,13 @@ alias cls="clear"
 alias rm='rm -i'
 #alias gcc='gcc -std=c99 -Wall'
 
+# cpp aliases
+alias g++20='g++-14 -std=c++20 -fmodules-ts -Wall -g'
+alias g++20h='g++-14 -std=c++20 -fmodules-ts -c -x c++-system-header'
+
+# cs246e setup
+source ~cs246e/setup
+
 function gcc() {
   if [[ $# -eq 2 ]]; then
     /usr/bin/gcc -std=c99 -Wall $1 -o $2
@@ -32,13 +39,22 @@ function marmosubmit() {
 function marmoresult() {
   if [[ "$1" == cs* ]]; then
     /u/cs_build/bin/marmoset results "$@"
-  else
+  else 
     /u/cs_build/bin/marmoset results cs136l "$@"
   fi
 }
 
-# colorful prompt
-PS1="\[\033[01;32m\]\u@\h:\[\033[01;34m\]\w \[\033[01;33m\]\t\[\033[00m\]\$ "
+# colorful prompt : user@host:cwd time [gitbranch] \n$
+if [ -f /usr/lib/git-core/git-sh-prompt ]; then
+  . /usr/lib/git-core/git-sh-prompt
+  PS1='\[\033[01;32m\]\u@\h:\[\033[01;34m\]\w \[\033[01;33m\]\t\[\033[01;36m\]$(__git_ps1 " (%s)")\[\033[00m\]\n\$ '
+else
+  # fallback PS1 if git-prompt not available
+  PS1='\[\033[01;32m\]\u@\h:\[\033[01;34m\]\w \[\033[01;33m\]\t\[\033[00m\]\n\$ '
+fi
+
+export PS1
+
 export PATH="$PATH:$HOME/bin"
 
 # messages for next login
@@ -47,3 +63,7 @@ RED='\033[0;31m'
 if [[ $- == *i* ]]; then
    echo -e "${RED}${MESSAGE}"
 fi
+
+function title () {
+    echo -ne "\033]0;$*\007"
+}
